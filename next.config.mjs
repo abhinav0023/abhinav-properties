9bae886bfd01447ac3f7b588b73210715d065520
Abhinav <abhinavhooda223@gmail.com>
2025-09-10 07:55:29 +0530

    css fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   reactStrictMode: true,
   output: "export", // needed for GitHub Pages
   images: {
     unoptimized: true, // fixes image loader issue
   },
-  assetPrefix: "https://abhinavproperties.com",
-  basePath: "",
 };
 
 export default nextConfig;
